4f25066794e493d5682e2ce6f4a4754134c8721d
CECK <az.ah.qwerty@gmail.com>
2022-10-23 17:40:05 +0500

    weather data model

--- src/app/instance.ts
+++ src/app/instance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const getBaseUrl = () => {
   switch (window.location.hostname) {
     default:
-      return 'https://api.openweathermap.org/data/2.5/forecast?lat=40&lon=40&appid=530d4e8348e4adc2839e9c6aa4a74d1b';
+      return 'http://api.openweathermap.org/data/2.5/forecast?lat=40&lon=40&appid=530d4e8348e4adc2839e9c6aa4a74d1b';
   }
 };
 
@@ -13,10 +13,5 @@ const instance = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
-export const getEpisodesByID = async (id: number) => {
-  const response = await axios.get(getBaseUrl + '/' + id);
-  return response.data;
-};
 
 export default instance;
---
+++ src/app/weather/types.ts
@@ -0,0 +1,31 @@
+export interface weatherModel{
+    
+        cod: string
+        message: string
+        cnt:number
+        list:[
+            temp: number,
+            feels_like:number ,
+            temp_min: number,
+            temp_max: number,
+            pressure: number,
+            sea_level: number,
+            grnd_level: number,
+            humidity: number,
+            temp_kf: number,
+        ]
+     weather:[
+        id:string,
+        main: string,
+        description:string,
+        icon: string
+     ]
+     clouds:[
+        all:number,
+
+     ]
+     wind:[
+          speed:number,
+     ]
+     visibility:string
+}
