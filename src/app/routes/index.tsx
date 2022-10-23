4e4673c76725f53eeadad67c73d659eb656d2803
CECK <az.ah.qwerty@gmail.com>
2022-10-23 21:04:37 +0500

    app routes

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const AppRouter: React.FC = () => {
+    return (
+        <div>
+            <Routes>
+                <Route />
+                <Route />
+
+            </Routes>
+        </div>
+    )
+}
+
+export default AppRouter
